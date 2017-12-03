b8a9afbfe20fa7c943a73246bcb4020f537c0084
areej941s <32181667+areej941s@users.noreply.github.com>
2017-12-03 13:40:10 -0800

    update

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ var counter = document.getElementById("counter");
 var icon = document.getElementById("icon");
 var house2 = document.getElementById("house2");
 var house3 = document.getElementById("house3");
+var house4 = document.getElementById("house4");
 var castle = document.getElementById("castle");
+var clouds = document.getElementById("clouds");
 
 
 window.onscroll = function(){
@@ -14,20 +16,16 @@ window.onscroll = function(){
 
   console.log(x);
 
-  counter.style.left = 400 + x * .97 + "px";
 
-  counter.innerHTML = x + "px";
 
-  if (x > 2000) {
-    counter.style.backgroundColor = "green";
-  }else{
-    counter.style.backgroundColor = "blue";}
 
-    icon.style.left = 100 + x  + "px";
 
-  if(x > 1000){
 
-  icon.src="https://2.bp.blogspot.com/-MTmoGMeE2cc/WSGIutFSHoI/AAAAAAAAK6o/Ya-tX6NvAqUM2LqV4VlxzsfwzD8xXEPFACLcB/s1600/254___thumbs-up-emoji.png";
+icon.style.left = 100 + x  + "px";
+
+  if(x > 900){
+
+  icon.src="img/castle1.png";
 
   }else{
 
@@ -36,24 +34,34 @@ window.onscroll = function(){
 
 house2.style.left = 500 + x  + "px";
 
-if(x > 1000){
+if(x > 5000){
 
-house2.src="https://2.bp.blogspot.com/-MTmoGMeE2cc/WSGIutFSHoI/AAAAAAAAK6o/Ya-tX6NvAqUM2LqV4VlxzsfwzD8xXEPFACLcB/s1600/254___thumbs-up-emoji.png";
+house2.src="img/castle2.png";
 
 }else{
 
   house2.src="img/house1.png";
 }
 
-house3.style.left = 800 + x  + "px";
+house3.style.left = 900 + x  + "px";
 
-if(x > 1000){
+if(x > 6000){
 
-house3.src="https://2.bp.blogspot.com/-MTmoGMeE2cc/WSGIutFSHoI/AAAAAAAAK6o/Ya-tX6NvAqUM2LqV4VlxzsfwzD8xXEPFACLcB/s1600/254___thumbs-up-emoji.png";
+house3.src="img/castle3.png";
 
 }else{
 
   house3.src="img/house1.png";
 }
+
+house4.style.left = 1300 + x  + "px";
+
+if(x > 8000){
+
+house4.src = "img/bigcastle.png";
+
+}else{
+
+house4.src="img/house1.png";}
 
 };
